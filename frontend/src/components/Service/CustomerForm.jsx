5bf936633ee3f2ae51b827aff322e022922bd863
Noin91 <ipexx257@proton.me>
2024-07-17 07:58:09 +0200

    new ServiceLayout for customersupport

---
+++ frontend/src/components/Service/CustomerForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Service/customerForm.module.css";
+
+const CustomerForm = () => {
+  return <div className={styles.container}>CustomerForm</div>;
+};
+
+export default CustomerForm;
