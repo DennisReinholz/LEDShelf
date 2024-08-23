57fef6f8f93a657163341b2c11492ec02bd13b18
itspecialistsimon <47610305+itspecialistsimon@users.noreply.github.com>
2024-08-23 21:21:12 +0200

    Make version visible for docker (#11)

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,19 +3,23 @@ import Logo from "../components/Logo.jsx";
 import MainNav from "./MainNav.jsx";
 import styles from "../styles/Sidebar/sidebar.module.css";
 
-const Sidebar = () => {
-  const [version, setVersion] = useState("");
-
-  useEffect(() => {
-    fetch("/package.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setVersion(data.version);
-      })
-      .catch((error) => {
-        console.error("Error fetching version:", error);
-      });
+useEffect(() => {
+  fetch("/package.json")
+  .then((response) => response.json())
+  .then((data) => {
+  setDevVersion(data.version);
+  })
+  .catch((error) => {
+  console.error("Error fetching version:", error);
+  });
   }, []);
+
+  const Sidebar = () => {
+  // Initialize state within the component
+  const [devVersion, setDevVersion] = useState("");
+  
+  // Use the injected version from the environment variable
+  const version = process.env.REACT_APP_VERSION || devVersion;
 
   return (
     <aside className={styles.container}>
