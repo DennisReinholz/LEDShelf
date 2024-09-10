4f0f381cfca6dc0403a4725481a9747e6685f1a9
Reinholz Dennis <90648017+Noin91@users.noreply.github.com>
2024-09-10 21:42:36 +0200

    feat: added logo and style (#23)

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "../styles/Sidebar/logo.module.css";
+import logo from "../assets/picture/Logo.jpg";
 
 const Logo = () => {
   return (
     <div className={styles.content}>
-      <div className={styles.logo}>
-        <p className={styles.name}>LedShelf</p>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="ledshelf logo" className={styles.logo} />
       </div>
+      <p className={styles.name}>LedShelf</p>
     </div>
   );
 };
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Logo from "../components/Logo.jsx";
 import MainNav from "./MainNav.jsx";
 import styles from "../styles/Sidebar/sidebar.module.css";
 
 const Sidebar = () => {
-
-useEffect(() => {
-  fetch("/package.json")
-  .then((response) => response.json())
-  .then((data) => {
-  setDevVersion(data.version);
-  })
-  .catch((error) => {
-  console.error("Error fetching version:", error);
-  });
+  useEffect(() => {
+    fetch("/package.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setDevVersion(data.version);
+      })
+      .catch((error) => {
+        console.error("Error fetching version:", error);
+      });
   }, []);
 
   // Initialize state within the component
   const [devVersion, setDevVersion] = useState("");
-  
+
   // Use the injected version from the environment variable
   const version = process.env.REACT_APP_VERSION || devVersion;
 
