5ee235dbafb407912887bf407c57cf4fd1d46b48
Noin91 <ipexx257@proton.me>
2024-10-05 21:37:36 +0200

    starting responsive design in shelf

--- frontend/src/components/Shelf/Shelf.jsx
+++ frontend/src/components/Shelf/Shelf.jsx
@@ -1,26 +1,50 @@
 import React from "react";
-import styles from "../../styles/Shelf/shelf.module.css";
 import { useNavigate } from "react-router-dom";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Modal from "../../components/common/Modal";
 import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap Styles
 
-const Shelf = ({ shelfname, place, shelfId }) => {
+const Shelf = ({ shelfname, place, shelfId, IsEdit, setIsEdit }) => {
   const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/regale/${shelfId}`);
+  };
+
+  const handleEdit = () => {
+    console.log("open Modal");
+  };
+
   return (
     <div
-      className={styles.container}
-      onClick={() => navigate(`/regale/${shelfId}`)}
+      className={`card ${IsEdit ? "border-warning" : "border-white"} mb-4`}
+      onClick={IsEdit ? handleNavigate : handleEdit}
+      style={{ width: "18rem", cursor: "pointer" }}
     >
-      <div className={styles.content}>
-        <p>{shelfname} </p>
-        <p>Ort: {place}</p>
+      <div className="card-body">
+        <div onClick={() => console.log(IsEdit)}>
+          <HiOutlineBars3 />
+        </div>
+        <h5 className="card-title">{shelfname}</h5>
+        <p className="card-text">Ort: {place}</p>
       </div>
+
+      {IsEdit && (
+        <Modal onClose={() => setIsEdit(false)}>
+          {/* Modal Inhalt */}
+        </Modal>
+      )}
     </div>
   );
 };
+
 Shelf.propTypes = {
   shelfname: PropTypes.node.isRequired,
   place: PropTypes.node.isRequired,
   shelfId: PropTypes.node.isRequired,
+  IsEdit: PropTypes.bool.isRequired,
+  setIsEdit: PropTypes.func.isRequired,
 };
 
 export default Shelf;
